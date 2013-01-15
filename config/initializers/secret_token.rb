912241d9d020bd6d05ca6833dfdc8a2ca8798df5
Matt Blackwell <mattblackwell@Matts-MacBook-Pro.local>
2013-01-15 10:57:47 -0600

    Guest Door Blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuestdoorBlog::Application.config.secret_token = '97d7806a527e83b22618e281a84bd41ea253f61782d871715aaf5a0878e1962f43cedd94757dddf9aa2adc139d4a356a8c41d504a9f364c133d70418205c456e'
